28d43c61e1d16de5593875a87bf509862f099598
Nirmal Baldaniya <nirmalb@codetailor.in>
2019-12-23 18:00:52 +0530

    feat: provide "trim" option into "htmlToText".

--- htmlToText.js
+++ htmlToText.js
@@ -3,12 +3,11 @@
  * Converts html text into plain text.
  * @param {String} html HTML Content
  */
-export const htmlToText = (html) => {
-  let d = document.getElementById('dwHtml2TextConverter')
-  if (!d) {
-    d = document.createElement('div');
-    d.id = 'dwHtml2TextConverter';
-  }
+export const htmlToText = (html = "", trim = false) => {
+  let d = document.createElement('div');
   d.innerHTML = html;
+  if (trim) {
+    return d.textContent.trim();
+  }
   return d.textContent;
 }
