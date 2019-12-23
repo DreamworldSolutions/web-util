9db5937b4fdaa4cd65a1060941f03da84d0cc6d6
Nirmal Baldaniya <nirmalb@codetailor.in>
2019-12-23 23:18:59 +0530

    feat: add utility method to detect touch device

---
+++ isTouchDevice.js
@@ -0,0 +1,9 @@
+
+/**
+ * Detect touch device.
+ */
+export const isTouchDevice = () => {
+  return ('ontouchstart' in window) ||
+    (navigator.maxTouchPoints > 0) ||
+    (navigator.msMaxTouchPoints > 0) ? true : false;
+}
