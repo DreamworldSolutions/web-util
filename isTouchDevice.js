04f665785d93db7e737a12c6ad4b3522c6f62304
Nirmal Baldaniya <nirmalb@codetailor.in>
2021-08-18 18:21:04 +0530

    fix(isTouchDevice): improve performance for touch device detection.

--- isTouchDevice.js
+++ isTouchDevice.js
@@ -1,13 +1,15 @@
-
+import once from 'lodash-es/once';
 import Bowser from "bowser";
 const bowser = Bowser.parse(window.navigator.userAgent);
+
 
 /**
  * Detect touch device.
  */
-export const isTouchDevice = () => {
-  return ('ontouchstart' in window) ||
-    (navigator.maxTouchPoints > 0) ||
-    (navigator.msMaxTouchPoints > 0) ||
-    bowser.platform.type !== 'desktop' ? true : false;
-}
+export const isTouchDevice = once(() => {
+    return ('ontouchstart' in window) ||
+      (navigator.maxTouchPoints > 0) ||
+      (navigator.msMaxTouchPoints > 0) ||
+      bowser.platform.type !== 'desktop' ? true : false;
+  }
+)
