35684497843f2f5b5e6ae4dc9460d08dd232065e
Nirmal Baldaniya <nirmalb@codetailor.in>
2021-08-26 11:06:54 +0530

    fix(isTouchDevice): Update logic of touch device detection. (#32)
    fix(isTouchDevice): Update logic of touch device detection.

--- isTouchDevice.js
+++ isTouchDevice.js
@@ -1,9 +1,18 @@
+import once from 'lodash-es/once';
+import Bowser from "bowser";
+const bowser = Bowser.parse(window.navigator.userAgent);
+
 
 /**
  * Detect touch device.
  */
-export const isTouchDevice = () => {
-  return ('ontouchstart' in window) ||
-    (navigator.maxTouchPoints > 0) ||
-    (navigator.msMaxTouchPoints > 0) ? true : false;
-}
+export const isTouchDevice = once(
+  () => {
+    if (bowser.platform.type === 'desktop') {
+      return false;
+    }
+    return ('ontouchstart' in window) ||
+      (navigator.maxTouchPoints > 0) ||
+      (navigator.msMaxTouchPoints > 0) ? true : false;
+  }
+)
