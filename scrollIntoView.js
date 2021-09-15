e5c38ab32c8fd5d63d94b53682680f9ba8be7e59
Nirmal Baldaniya <nirmalb@codetailor.in>
2021-09-15 14:59:45 +0530

    fix(scrollIntoView): log error when scrollingElement or element is not found. (#34)
    fix(scrollIntoView): log error when scrollingElement or element is not provided.

--- scrollIntoView.js
+++ scrollIntoView.js
@@ -11,9 +11,13 @@
  * @param {Number} offsetTop top offset.
  * @param {Number} offsetBottom bottom offset.
  */
- export const scrollIntoView = (scrollingElement, element, bottom = false, offsetTop = 0, offsetBottom = 0) => {
-  
-   const intersectionCallback = (entries) => {
+export const scrollIntoView = (scrollingElement, element, bottom = false, offsetTop = 0, offsetBottom = 0) => {
+  if (!scrollingElement || !element) {
+    console.error('scrollIntoView: scrollingElement or element is not provided.', scrollingElement, element);
+    return;
+  }
+
+  const intersectionCallback = (entries) => {
     intersectionInstance && intersectionInstance.disconnect();
     intersectionInstance = null;
 
