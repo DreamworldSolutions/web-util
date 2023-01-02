e0c6fca623188923937231a3ef8458fa599fd81e
shakil-mansuri-dream <53848663+shakil-mansuri-dream@users.noreply.github.com>
2023-01-02 12:36:30 +0530

    feat: add replaceAll method. (#47)
    card link: https://a.kerika.net/C7r/board/N2O6/Zwkd
    
    Co-authored-by: Shakil Mansuri <shakil.mansuri@dreamworldsolutions.com>

---
+++ replaceAll.js
@@ -0,0 +1,29 @@
+import replace from 'lodash-es/replace';
+
+/**
+ * Why is it needed?: There is no support of replaceAll method in old browsers,
+ *                    so it is its wrapper method which if browser's replaceAll method is available,
+ *                    it will use it, otherwise it will replace with custom logic using lodash's replace method.
+ *
+ * Replaces matches for pattern in string with replacement.
+ * @param {String} string The string to modify.
+ * @param {String} word The pattern to replace.
+ * @param {String} replacement The match replacement.
+ * @returns {String} Returns the modified string.
+ */
+export const replaceAll = (string, word, replacement) => {
+  const browserReplaceAllFeature = ''.replaceAll && typeof ''.replaceAll == 'function';
+  if(browserReplaceAllFeature){
+    return string.replaceAll(word, replacement);
+  }
+
+  let resultText;
+  while(true) {
+    resultText = replace(string, word, replacement);
+    if (resultText === text) {
+      break;
+    }
+    text = resultText;
+  }
+  return resultText;
+}
